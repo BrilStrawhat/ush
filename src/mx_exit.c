1c18e5579c5b6405a57dc3c6154e818be86fef80
Bohdan Dotsenko <bdotsenko@e1r2p5.unit.ua>
2020-06-11 22:12:38 +0300

    fix exit

--- src/mx_exit.c
+++ src/mx_exit.c
@@ -1,13 +1,28 @@
 #include "ush.h"
 
+static bool is_dig_str(char *str) {
+    int i;
+
+    for (i = 0; str[i] && isdigit(str[i]); i++);
+    if (!str[i])
+        return true;
+    return false;
+
+}
+
 int mx_exit(st_launch *l_inf) {
     int *exit_st = mx_exit_status();
 
     if (!l_inf->cmd_arr[1]) {
         exit(*exit_st);
-    }  
-    if (l_inf->cmd_arr[1] && isdigit(atoi(l_inf->cmd_arr[1])))
+    }
+    if (l_inf->cmd_arr[2]){
+        mx_printerr("exit: too many arguments\n");
+        return 1;
+    }
+    if (l_inf->cmd_arr[1] && is_dig_str(l_inf->cmd_arr[1])) {
         exit(atoi(l_inf->cmd_arr[1]));
+    }
     
     return 0;
 }
